3cfa0ecfd675408d94e5c3c870e789b4d1522785
Marcos <marcospcsouza1802@gmail.com>
2023-09-26 20:48:41 -0300

    pitch

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-/* function showSubmit() {
+function showSubmit() {
     document.getElementById('tela6').style.display = 'block';
     document.getElementById('btn-vsl').style.display = 'block';
     document.getElementById('btn-caminho').style.display = 'block';
     document.getElementById('btn-depoimentos').style.display = 'block';
+    document.getElementById('preco-container').style.display = 'flex';
 }
 
-setTimeout(showSubmit, 125000);*/
+setTimeout(showSubmit, 118000);
